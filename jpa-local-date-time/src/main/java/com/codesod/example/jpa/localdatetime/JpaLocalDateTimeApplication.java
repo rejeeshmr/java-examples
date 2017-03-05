fe706a8537f6b34bf4820d35905017ac172d9860
sayembd <sayem64@gmail.com>
2017-03-05 21:09:48 +0100

    Initial commit

---
+++ jpa-local-date-time/src/main/java/com/codesod/example/jpa/localdatetime/JpaLocalDateTimeApplication.java
@@ -0,0 +1,12 @@
+package com.codesod.example.jpa.localdatetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLocalDateTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLocalDateTimeApplication.class, args);
+	}
+}
---
+++ jpa-local-date-time/src/test/java/com/codesod/example/jpa/localdatetime/JpaLocalDateTimeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codesod.example.jpa.localdatetime;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaLocalDateTimeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
